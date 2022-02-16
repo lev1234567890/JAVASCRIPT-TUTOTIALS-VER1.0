666d86e4cc7be48be7d855429484c767b46d8eb2
Leva <97125651+lev1234567890@users.noreply.github.com>
2022-02-16 14:55:39 +0300

    Add files via upload

---
+++ onclick.js
@@ -0,0 +1,8 @@
+// GitHub user: lev1234567890
+// E-email: levbulychou@gmail.com
+// YouTube user: Лёва
+let element = document.querySelector('.element');
+let btn = document.querySelector('.btn');
+btn.onclick = function (){
+    element.classList.toggle('test');
+};
